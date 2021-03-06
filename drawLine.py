b0187cc9cdd27246e4f835ac3c4198c5d1041d4e
Brook <z-jt@hotmail.com>
2021-03-06 13:02:01 +0800

    fix run error

--- drawLine.py
+++ drawLine.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import constant
 
 def drawLines(x,y,name,color,titlename,fileName):
-    plt.figure(dpi=drawDpi)
+    plt.figure(dpi=constant.drawDpi)
     for i in range(0, len(y)):
         plt.plot(x, y[i], label= name[i], linewidth=1, color = color[i])
     #plt.xlabel('date')
     #plt.ylabel('value')
     plt.title(titlename)
     plt.legend()
-    plt.savefig(fileName + '.' + fileType, dpi=drawDpi, format=fileType, transparent=False, bbox_inches=None, pad_inches=0.1, metadata=None)
+    plt.savefig(fileName + '.' + constant.fileType, dpi=constant.drawDpi, format=constant.fileType, transparent=False, bbox_inches=None, pad_inches=0.1, metadata=None)
     #plt.show()
